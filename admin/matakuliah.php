f0b2aace2c6972d3c977da53c97544dccc3f8d19
wakhid <wakhid.28@gmail.com>
2014-01-28 00:50:09 +0700

    update 2
    rubah value MK dari P ke )

--- admin/matakuliah.php
+++ admin/matakuliah.php
@@ -42,7 +42,7 @@
 				<option value="6">6</option>
 				<option value="7">7</option>
 				<option value="8">8</option>
-				<option value="P">Pilihan</option>
+				<option value="0">MK Pilihan</option>
 			</select>
 		</td>
 	</tr>
